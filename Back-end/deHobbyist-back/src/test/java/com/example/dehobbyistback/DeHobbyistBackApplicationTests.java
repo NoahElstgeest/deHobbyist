d432686f01755681450f3387be004c08debaa897
Noah Elstgeest <s1136032@student.hsleiden.nl>
2022-12-16 15:02:32 +0100

    Project creation
    This commit creates the project

---
+++ Back-end/deHobbyist-back/src/test/java/com/example/dehobbyistback/DeHobbyistBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dehobbyistback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeHobbyistBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
