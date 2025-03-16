8c94fa7b9742b7bd14498d70f62c76a574625524
Noah Elstgeest <s1136032@student.hsleiden.nl>
2025-03-16 14:34:39 +0100

    FEAT: Added some styling and a banner image

--- Front-end/deHobbyist-front/src/app/home/home.component.ts
+++ Front-end/deHobbyist-front/src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  featuredProducts = [
+    { name: 'Product 1', price: 19.99, image: 'assets/product1.jpg' },
+    { name: 'Product 2', price: 29.99, image: 'assets/product2.jpg' },
+    { name: 'Product 3', price: 39.99, image: 'assets/product3.jpg' }
+  ];
 }
