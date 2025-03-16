51fc379a30bdf6f10618572fa959e666458cc4b7
Noah Elstgeest <s1136032@student.hsleiden.nl>
2025-03-16 15:10:41 +0100

    FEAT: Added product modal component

---
+++ Front-end/deHobbyist-front/src/app/products/product-details-modal/product-details-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details-modal',
+  imports: [],
+  templateUrl: './product-details-modal.component.html',
+  styleUrl: './product-details-modal.component.scss'
+})
+export class ProductDetailsModalComponent {
+
+}
