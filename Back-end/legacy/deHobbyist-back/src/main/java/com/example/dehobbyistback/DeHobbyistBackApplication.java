3faebbd2add0ba8b41fca43df3d68dae551f2985
Noah Elstgeest <s1136032@student.hsleiden.nl>
2025-03-18 21:22:12 +0100

    Updated back-end to newest version

---
+++ Back-end/legacy/deHobbyist-back/src/main/java/com/example/dehobbyistback/DeHobbyistBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.dehobbyistback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeHobbyistBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeHobbyistBackApplication.class, args);
+    }
+
+}
