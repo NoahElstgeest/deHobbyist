64eeb71ce6a9779e76056af0516a1f66da42b793
Noah Elstgeest <s1136032@student.hsleiden.nl>
2025-10-26 21:06:10 +0100

    FEAT: Styling as well as registering users now works as well as the link to take you to the login page now works.

--- Front-end/deHobbyist-front/src/app/register/register.component.ts
+++ Front-end/deHobbyist-front/src/app/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import {ApiService} from '../api.service';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -14,18 +14,31 @@ export class RegisterComponent {
   username: string = '';
   email: string = '';
   password: string = '';
+  confirmPassword: string = '';
   errorMessage: string = '';
   successMessage: string = '';
 
   constructor(private apiService: ApiService, private router: Router) {}
 
   register(): void {
+    if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'Wachtwoorden komen niet overeen.';
+      this.successMessage = '';
+      return;
+    }
+
+    if (!this.username || !this.email || !this.password) {
+      this.errorMessage = 'Vul alle velden in.';
+      this.successMessage = '';
+      return;
+    }
+
     this.apiService.register(this.username, this.email, this.password).subscribe({
       next: () => {
-        this.successMessage = 'Registration successful! Redirecting to login...';
+        this.successMessage = 'Registratie successvol! We leiden je nu om...';
         setTimeout(() => this.router.navigate(['/login']), 2000);
       },
-      error: () => this.errorMessage = 'Registration failed. Try again.'
+      error: () => this.errorMessage = 'Registratie gefaald. Probeer later opnieuw.'
     });
   }
 }
