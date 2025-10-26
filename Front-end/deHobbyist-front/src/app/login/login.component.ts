d69f8bcdf09b3d3f405b55cecb176ef39f3232e7
Noah Elstgeest <s1136032@student.hsleiden.nl>
2025-10-26 21:05:29 +0100

    FEAT: Styling as well as login functionality now working correctly

--- Front-end/deHobbyist-front/src/app/login/login.component.ts
+++ Front-end/deHobbyist-front/src/app/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import {ApiService} from '../api.service';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -18,10 +18,12 @@ export class LoginComponent {
   constructor(private apiService: ApiService, private router: Router) {}
 
   login(): void {
+    console.log(this.password, this.username);
     this.apiService.login(this.username, this.password).subscribe({
       next: (response) => {
         localStorage.setItem('token', response.token);
         localStorage.setItem('role', response.role);
+        localStorage.setItem('userId', String(response.id));
         this.router.navigate(['/']);
       },
       error: () => this.errorMessage = 'Invalid username or password'
