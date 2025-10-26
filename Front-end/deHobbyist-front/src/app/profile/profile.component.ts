0e3eba215617d16f02675da41d8cc71c2c2781d7
Noah Elstgeest <s1136032@student.hsleiden.nl>
2025-10-26 21:07:07 +0100

    FEAT: Styling as well as added boxes to enter and save shipping and billing addresses. A view order button has also been addded.

--- Front-end/deHobbyist-front/src/app/profile/profile.component.ts
+++ Front-end/deHobbyist-front/src/app/profile/profile.component.ts
@@ -1,29 +1,57 @@
 import {Component, OnInit} from '@angular/core';
-import {ApiService, Order} from '../api.service';
+import {ApiService} from '../api.service';
 import {CommonModule} from '@angular/common';
+import {User} from '../models/user';
+import {RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent implements OnInit {
-  orders: Order[] = [];
-  errorMessage: string = '';
-  username: string = '';
+  me: User | null = null;
+  loading = true;
+  saving = false;
+  error: string | null = null;
+  saveMessage: string | null = null;
+  saveError: string | null = null;
 
-  constructor(private apiService: ApiService) {}
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
-    this.username = localStorage.getItem('username') || 'User';
-    this.loadOrders();
+    this.api.getMe().subscribe({
+      next: (data) => {
+        this.me = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Kon profielinformatie niet ophalen.';
+        this.loading = false;
+      }
+    });
   }
 
-  loadOrders(): void {
-    this.apiService.getUserOrders().subscribe({
-      next: (data) => this.orders = data,
-      error: () => this.errorMessage = 'Error fetching orders'
+  saveAddresses(): void {
+    if (!this.me) return;
+    this.saving = true;
+    this.saveMessage = null;
+    this.saveError = null;
+
+    this.api.updateUser(this.me.id, {
+      shippingAddress: this.me.shippingAddress,
+      billingAddress: this.me.billingAddress
+    }).subscribe({
+      next: () => {
+        this.saveMessage = 'adres bijgewerkt.';
+        this.saving = false;
+      },
+      error: () => {
+        this.saveError = 'Opslaan mislukt. Probeer het later opnieuw.';
+        this.saving = false;
+      }
     });
   }
 }
